48f5227f0459770ace57a646d733cc397e4c48cf
Siheon Kim <siheon0411@gmail.com>
2024-01-18 18:11:08 +0900

    LRA failed

--- 2.LRA/LRA/LRA.ino
+++ 2.LRA/LRA/LRA.ino
@@ -17,30 +17,36 @@ short sinewave[128] = { 245, 248, 250, 251, 253, 254, 254, 255,
                       };
 
 int LRA_PIN = DAC1; // DAC1 핀을 사용하여 LRA 연결
+int FREQ = 100; // 목표진동수 100Hz
 int UPDATE_RATE = 1000; // 업데이트 속도 (Hz)
-const int NUM_SAMPLES = 128; // 룩업 테이블의 데이터 수
+int rate = UPDATE_RATE / FREQ;
+const int NUM = 128; // 룩업 테이블의 데이터 수
+
+int idx = 0;
 
 void setup() {
-// DAC 설정
-  analogWriteResolution(8); // 아날로그 출력 해상도 설정 (8비트)
-  analogWrite(LRA_PIN, 128); // 초기 출력 값 설정 (0에서 255 사이의 중간값)  
-  Serial.begin(115200);
-  Serial.println("start");
-
+  // analogWriteResolution(8); // 아날로그 출력 해상도 설정 (8비트: 0~255)
+  // Serial.begin(115200);
+  // Serial.println("start");
 }
 
 void loop() {
-  //unsigned long startTime = micros(); // 시작 시간 기록
+  // for (int i = 0; i < NUM; i++) {
+  //   int outputValue = sinewave[i];
+  //   analogWrite(LRA_PIN, outputValue); // 사인파 출력 값을 DAC에 출력
+  //   // Serial.println(outputValue);  // 출력 값 그리기용 프린트
+  //   delay(10000 / NUM / FREQ);  // Delay 시간 계산
+  // }
 
-  for (int i = 0; i < NUM_SAMPLES; i++) {
-    int outputValue = map(sinewave[i], -128, 127, 0, 255);
-    analogWrite(LRA_PIN, outputValue); // 변환된 값을 DAC에 출력
-    Serial.println(outputValue);
-    // delayMicroseconds(1000000 / UPDATE_RATE); // Delay 시간 계산
-    delay(1000 / UPDATE_RATE);
+  int outputValue = sinewave[idx];
+  analogWrite(LRA_PIN, outputValue);
+  delay(1000 / FREQ);
+  idx++;
+
+  if (idx > 127) {
+    idx = 0;
   }
 
-  //unsigned long elapsedTime = micros() - startTime; // 경과 시간 계산
   // 필요한 경우 시간을 조정하여 정확한 주파수를 맞출 수 있음
-  // 오실로스코프로 생성된 신호를 확인할 수 있음
+  // 오실로스코프로 생성된 신호를 확인
 }
